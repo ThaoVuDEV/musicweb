6652123199c8a391eeaeeee7c54ea9ce2181c33b
ThaoDev17 <thaovu.dev17@gmail.com>
2024-04-01 22:01:41 +0700

    cc

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Album extends Model
 {
     use HasFactory;
+    
+    protected $fillable=[
+        'title',
+        'artist_id',
+        'release_year'
+    ];
+    public function artist(){
+        return $this->belongsTo(Artist::class);   
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    //return view('client.pages.home');
-    return view('client.components.shop.shop');
-});
+// Route::get('/', function () {
+//     //return view('client.pages.home');
+//     return view('client.components.shop.shop');
+// });
+Route::get('', HomeController::class.'@index');
